91729b68e2e9929d9c035dce0d2134e6cd42698b
Alex Kireev <a.kireev@g.nsu.ru>
2019-01-29 19:40:51 +0700

    Added SshClient class

--- TodoApi/Controllers/LoginController.cs
+++ TodoApi/Controllers/LoginController.cs
@@ -27,52 +27,7 @@ namespace comptech2019_ict.Controllers
         [HttpPost("[action]")]
         public string Auth([FromBody] User user)
         {
-            const int Port = 2231;
-            const string ServerName = "ssd1.sscc.ru";
-            const string Charset = "utf-8";
-
-            var ssh = new Chilkat.Ssh();
-
-            bool success = UnlockSSH(ssh);
-            if(success != true)
-            {
-                return ssh.LastErrorText + "\nUnlock SSH failed";
-            }
-
-            success = ssh.Connect(ServerName, Port);
-            if (success != true)
-            {
-                return ssh.LastErrorText + "\nConnection failed";
-            }
-
-            success = ssh.AuthenticatePw(user.Name, user.Password);
-            if(success != true)
-            {
-                Console.WriteLine(ssh.LastErrorText + "\nUnlock SSH failed");
-                return ssh.LastErrorText + "\nAuthenticate failed";
-            }
-
-            string strOutput = ssh.QuickCommand("ls", Charset);
-            if (ssh.LastMethodSuccess != true)
-            {
-                Console.WriteLine(ssh.LastErrorText);
-                return ssh.LastErrorText + "\nCommand failed";
-            }
-            Console.WriteLine(ssh.LastErrorText);
-
-            return strOutput;
-        }
-
-        private bool UnlockSSH(Chilkat.Ssh ssh)
-        {
-            bool success = ssh.UnlockComponent("Unlockcode");
-            if (success != true)
-            {
-                Console.WriteLine(ssh.LastErrorText + "\nUnlock failed");
-                return success;
-            }
-            Console.WriteLine(ssh.LastErrorText + "\nUnlock succesful");
-            return success;
+            return "Hello";
         }
     }
 }
---
+++ TodoApi/SshClient.cs
@@ -0,0 +1,47 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi
+{
+    public class SshClient
+    {
+        //readonly int Port;
+        //readonly string ServerName;
+        //public string Charset { get; set; }
+
+        readonly ConnectionInfo ConnectionInfo;
+        private readonly Renci.SshNet.SshClient Ssh;
+        
+        SshClient(string ServerName, int Port, string Charset, string Name, string Password)
+        {
+            //this.Port = Port;
+            //this.ServerName = ServerName;
+            //this.Charset = Charset;
+            ConnectionInfo = new ConnectionInfo(ServerName, Port, Name,
+                new AuthenticationMethod[]{
+                    new PasswordAuthenticationMethod(Name, Password)
+                    /*,new PrivateKeyAuthenticationMethod(rsaKey)*/
+                });
+            Ssh = new Renci.SshNet.SshClient(ConnectionInfo);
+        }
+
+        public void Connect()
+        {
+            Ssh.Connect(); // эта штука кидается исключениями
+        }
+
+        public string ThrowCommand(string Command)
+        {
+            return Ssh.CreateCommand(Command).Execute(); // эта штука тоже кидается исключениями
+        }
+
+        private void Disconnect()
+        {
+            if(Ssh.IsConnected)
+                Ssh.Disconnect();
+        }
+    }
+}
